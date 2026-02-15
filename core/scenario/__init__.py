babf92d1f8318ea3b5b0e21841e04062d917cb73
Thanh Le <hi@thanhle.xyz>
2026-02-15 20:04:46 +0900

    updates

--- core/scenario/__init__.py
+++ core/scenario/__init__.py
@@ -1,3 +1,2 @@
-from .visualize_channel import visualize_channel
 from .prep_data import prep_data
 from .fit import fit
---
+++ core/scenario/plot_h_distribution.py
@@ -0,0 +1,10 @@
+
+def plot_h_distribution(args):
+    '''
+    Refer to scenario/fit.py, then load `channel_io.npz`.
+    Extract h then scatter plot real and imag part of h.
+    Also, sample args.n_prep h_hat from model at `channel.pt`. Then scatter plot the real and imag part of h_hat.
+    Save the plot.
+
+    Also, in a separate file, plot the channel.pt
+    '''
